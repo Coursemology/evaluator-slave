74d7ccceeb2aaffd4574c7f7a241476d1e18dadd
Joel Low <joel@joelsplace.sg>
2016-01-20 21:41:40 +0800

    Implement a trivial Docker::Image cache.
    This prevents repeated hits to Docker Hub when running the specs. This *does not* affect production runs.

---
+++ spec/support/docker_images.rb
@@ -0,0 +1,11 @@
+# This caches the results of pulling from Docker Hub for the duration of the test session.
+module DockerImageCache
+  def create(hash)
+    return super unless hash['fromImage']
+
+    @cache ||= {}
+    @cache[hash['fromImage']] ||= super
+  end
+end
+
+Docker::Image.singleton_class.send(:prepend, DockerImageCache)
