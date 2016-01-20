c03789921aa408de7caa426948a9971d02033c6b
Joel Low <joel@joelsplace.sg>
2016-01-20 17:56:28 +0800

    Define the behaviour of the evaluator when no test report was generated.

--- spec/coursemology/evaluator/services/evaluate_programming_package_service_spec.rb
+++ spec/coursemology/evaluator/services/evaluate_programming_package_service_spec.rb
@@ -75,19 +75,27 @@ RSpec.describe Coursemology::Evaluator::Services::EvaluateProgrammingPackageServ
     let(:image) { 'python:2.7' }
     let(:report_path) { File.join(__dir__, '../../../fixtures/sample_report.xml') }
     let(:report_contents) { File.read(report_path) }
-    let(:container) do
-      container = subject.send(:create_container, image)
+    let(:container) { subject.send(:create_container, image) }
+
+    def copy_dummy_report
       container.start!
       container.wait
       tar = StringIO.new(Docker::Util.create_tar('report.xml' => report_contents))
       container.archive_in_stream(Coursemology::Evaluator::Services::
-        EvaluateProgrammingPackageService::PACKAGE_PATH) do
+          EvaluateProgrammingPackageService::PACKAGE_PATH) do
         tar.read
       end
     end
 
     it 'returns the test report' do
+      copy_dummy_report
       expect(subject.send(:extract_test_report, container)).to eq(report_contents)
+    end
+
+    context 'when running the tests fails' do
+      it 'returns nil' do
+        expect(subject.send(:extract_test_report, container)).to be_nil
+      end
     end
   end
 end
